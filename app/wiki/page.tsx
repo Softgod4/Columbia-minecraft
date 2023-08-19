052dc083a15aa78282842b8616c8ffd2cb805185
Softgod4 <softgod2005@gmail.com>
2023-08-19 18:00:12 +0300

    убрана поддержка разработчика

--- app/wiki/page.tsx
+++ app/wiki/page.tsx
@@ -7,12 +7,12 @@ export default function Wiki() {
                 <img src="/big-icon.png" alt="" />
                 <h2 className="h2_title mt-12 mb-24">Вики находится в разработке</h2>
 
-                <div>
+                {/* <div>
                     <h2 className="h2_title mb-4">Поддержать разработчика:</h2>
 
                         <p className={styles.sber}>Сбер: 2202 2036 5280 0865 (Александр Б.)</p>
                         <p className={styles.tinkoff}>Тинькофф: 2200 7004 9726 5388 (Александр Б.)</p>
-                </div>
+                </div> */}
             </div>
         </section>
     )
